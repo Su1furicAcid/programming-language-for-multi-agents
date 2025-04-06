a18734cc20f22c2afac01a867a415d40fa08cbc4
孙奥 <115922135+Su1furicAcid@users.noreply.github.com>
2025-04-06 20:53:20 +0800

    update: update parser

--- parser.py
+++ parser.py
@@ -9,3 +9,25 @@ precedence = (
     ('left', 'EQ', 'NEQ', 'GT', 'LT', 'LE', 'GE'),
 )
 
+def p_program(p):
+    '''program : global_block program_body
+               | program_body'''
+    if len(p) == 3:
+        p[0] = Program(global_block=p[1], body=p[2])
+    else:
+        p[0] = Program(global_block=None, body=p[1])
+
+def p_program_body(p):
+    '''program_body : statement program_body
+                    | agent_def program_body
+                    | connect_block program_body
+                    | func_def program_body
+                    | empty'''
+    if len(p) == 3:
+        p[0] = [p[1]] + p[2]
+    else:
+        p[0] = []
+
+def p_global_block(p):
+    '''global_block : GLOBAL COLON INDENT var_decl_list DEDENT'''
+    p[0] = GlobalBlock(variables=p[4])
